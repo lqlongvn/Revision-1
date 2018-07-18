b56c4836afd8631095891286941aca12912adc22
Long <lqlongvn@gmail.com>
2018-07-18 23:31:13 +0700

    GameCanvas; paintComponent;

---
+++ src/GameCanvas.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCanvas extends JPanel {
+
+    public GameCanvas(){
+        this.setSize(1024,600);
+        this.setVisible(true);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+        g.setColor(Color.cyan);
+        g.fillRect(0,0,40,50);
+    }
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
+
+    GameCanvas gameCanvas;
+
     public GameWindow(){
         this.setSize(1024,600);
         this.setVisible(true);
+        this.gameCanvas = new GameCanvas();
+        this.add(this.gameCanvas);
     }
 }
