24a6788ce20cda171fffcae347dc360c69820d9d
Long <lqlongvn@gmail.com>
2018-07-19 22:27:37 +0700

    Load starImage

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,11 +1,22 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GameCanvas extends JPanel {
+
+    BufferedImage starImage;
 
     public GameCanvas(){
         this.setSize(1024,600);
         this.setVisible(true);
+        try {
+            this.starImage = ImageIO.read(new File("resources/images/star.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
