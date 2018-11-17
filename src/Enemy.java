059199d0f2862ea835eacd70d04ca691c162b12f
Long <lqlongvn@gmail.com>
2018-11-17 16:08:37 +0700

    Random X of Enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,22 +1,33 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Enemy {
     public BufferedImage image;
     public int x;
     public int y;
+    public int velocityX, velocityY;
     public int width;
     public int height;
+    private Random random1 = new Random();
+
 
     public Enemy () {
-        this.x = 300;
+        this.x = this.random1.nextInt(600);
         this.y = 10;
+        this.velocityX = 2;
+        this.velocityY = 3;
         this.width = 15;
         this.height = 15;
     }
 
     public void render(Graphics graphics) {
         graphics.drawImage(this.image, this.x, this.y, this.width, this.height, null);
+    }
+
+    public void run() {
+        this.x += this.velocityX;
+        this.y += this.velocityY;
     }
 
 }
